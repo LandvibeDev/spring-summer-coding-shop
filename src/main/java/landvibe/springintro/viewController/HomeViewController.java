1f1b0998fbbf8099ba66d86c14a4d692b94dafff
jgw1202 <jgw1202@naver.com>
2024-07-22 13:48:32 +0900

    refactor : 패키지 구조 변경

--- src/main/java/landvibe/springintro/viewController/HomeViewController.java
+++ src/main/java/landvibe/springintro/viewController/HomeViewController.java
@@ -1,9 +1,9 @@
-package landvibe.springintro.controller;
+package landvibe.springintro.viewController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
-public class HomeController {
+public class HomeViewController {
     @GetMapping("/")
     public String home() {
         return "home";
